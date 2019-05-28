03f79f207b8a47bca45c9a61dbc510ea988766b8
chengyongjian <cyjian0@gmail.com>
2019-05-28 18:34:14 +0800

    insert db success

--- house/test.py
+++ house/test.py
@@ -5,66 +5,55 @@ from sqlalchemy import create_engine
 import time
 import json
 import house.settings
+from house.house_model import House
 
 engine = create_engine("mysql+pymysql://root:macbook@localhost:3306/spider", max_overflow=5)
-
 Base = declarative_base()
 time_string = time.strftime("%Y%m%d", time.localtime())
 
-# 创建单表
+#创建单表
 class House(Base):
-	
-    __tablename__ = time_string
-    id = Column(Integer, primary_key=True)
-    title = Column(String(64))
-    link = Column(String(64))
-    area = Column(String(64))
-    location = Column(String(64))
-    totalPrice = Column(String(64))
-    unitPrice = Column(String(64))
-    flood = Column(String(64))
-    follow = Column(String(64))
-    tag1 = Column(String(64))
-    tag2 = Column(String(64))
-    
-#定义初始化数据库函数
-def init_db():
-    Base.metadata.create_all(engine)
+	__tablename__ = time_string
+	id = Column(Integer, primary_key=True)
+	title = Column(String(64))
+	link = Column(String(64))
+	area = Column(String(64))
+	location = Column(String(64))
+	totalPrice = Column(String(64))
+	unitPrice = Column(String(64))
+	flood = Column(String(64))
+	follow = Column(String(64))
+	tag1 = Column(String(64))
+	tag2 = Column(String(64))
 
-#顶固删除数据库函数
+def init_db():
+	Base.metadata.create_all(engine)
+
 def drop_db():
-    Base.metadata.drop_all(engine)
-    
-def insert_data(data_json):
-	insert_data = []
+	Base.metadata.drop_all(engine)
+	
+def insert_data_many(insert_data):
 	Session = sessionmaker(bind=engine)
 	session = Session()
-	for item in data_json:
-		insert_data.append(
-			House(
-				title = item['title'],
-				link = item['link'],
-				area = item['area'],
-				location = item['location'],
-				totalPrice = item['totalPrice'],
-				unitPrice = item['unitPrice'],
-				# flood = item['flood'],
-				follow = item['follow'],
-				# tag1 = item['tag1'],
-				# tag2 = item['tag']
-			)
-		)
-		
 	session.add_all(insert_data)
 	session.commit()
 
-# def read_json(json_name):
-# 	with open(json_name, 'r', encoding='utf-8') as f:
-# 		data_json = json.load(f)
-# 		f.close()
-# 	return data_json
 
-# if __name__ == '__main__':
-#     init_db()
-#     data = read_json("./data/20190520_hours.json")
-#     insert_data(data)
+def insert_data_one_by_one(insert_data):
+	Session = sessionmaker(bind=engine)
+	session = Session()
+	session.add(insert_data)
+	session.commit()
+
+
+if __name__ == '__main__':
+	# def create_db():
+	# hous_s = House()
+	# hous_s.title = "title"
+	# hous_s.area = "area"
+	# insert_data_one_by_one(hous_s)
+    init_db()
+    # data = read_json("./data/20190520_hours.json")
+    # insert_data(data)
+    
+    
