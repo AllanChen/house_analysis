e709bde3e54c6f65d4a7ff087edc08123a15f06e
Allanchan <cyjian0@gmail.com>
2019-05-29 08:14:54 +0800

    prepare to connect remote databases

--- house/pipelines.py
+++ house/pipelines.py
@@ -14,14 +14,14 @@ class HousePipeline(object):
     def process_item(self, item, spider):
         return item
 
-class JsonWithEncodingHouseapeline(object):
+class DataBaseWithEncodingHousepeline(object):
     def __init__(self):
         self.insert_house_data = []
         time_string = house.settings.DATA_NAME
         file_name = time_string + "_hours.txt"
         self.file = codecs.open(file_name, 'wb', 'utf-8')
         house.house_model.init_db()
-    
+
     def process_item(self, item, spider):
         self.insert_house_data.clear()
         for i in range(1, len(item['title'])):
@@ -34,9 +34,65 @@ class JsonWithEncodingHouseapeline(object):
             insert_h.totalPrice = str(item['totalPrice'][i])
             insert_h.unitPrice = str(item['unitPrice'][i])
             self.insert_house_data.append(insert_h)
-            
+
         house.house_model.insert_data_many(self.insert_house_data)
         return item
-    
+
     def spider_closed(self, spider):
         self.file.close()
+
+
+class JsonWithEncodingLianjiapeline(object):
+    def __init__(self):
+        time_string = house.settings.DATA_NAME
+        file_name = time_string + "_hours.txt"
+        self.file = codecs.open(file_name, 'wb', 'utf-8')
+        house.test.init_db()
+
+    def process_item(self, item, spider):
+        title_list = item['title']
+        for i in range(1, len(title_list)):
+            line = json.dumps(
+                {
+                    "title": str(item['title'][i]),
+                    'area': str(item['area'][i]),
+                    'link': str(item['link'][i]),
+                    'location': str(item['location'][i]),
+                    # 'tag1' : str(item['tag1'][i]) ,
+                    # 'tag2': str(item['tag2'][i]),
+                    'follow': str(item['follow'][i]),
+                    'totalPrice': str(item['totalPrice'][i]),
+                    'unitPrice': str(item['unitPrice'][i]),
+                }, ensure_ascii=False)
+            line += ","
+            self.file.write(line)
+
+        time_string = house.settings.DATA_NAME
+        file_name = time_string + "_hours.txt"
+        self.txt_to_json(file_name)
+        return item
+
+    def txt_to_json(self, txt_name):
+        data = ""
+        time_string = house.settings.DATA_NAME
+        with open(txt_name, 'r', encoding='utf-8') as file:
+            data = file.read()
+
+        data = "[" + data
+        data = data + "]"
+        data = data.replace(",]", "]")
+        json_save_name = time_string + "_hours.json"
+        file = codecs.open(json_save_name, 'wb', 'utf-8')
+        file.write(data)
+
+        with open(json_save_name, 'r', encoding='utf-8') as f:
+            data_json = json.load(f)
+            f.close()
+
+        house.test.insert_data(data)
+
+    def spider_closed(self, spider):
+        self.file.close()
+
+
+
--- house/test.py
+++ house/test.py
@@ -1,20 +1,18 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint, Index
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-import time
 import json
+import codecs
 import house.settings
-from house.house_model import House
 
-engine = create_engine("mysql+pymysql://root:macbook@localhost:3306/spider", max_overflow=5)
+engine = create_engine("mysql+pymysql://root:zPSsZYtmmjAJAcjR@39.105.97.84:3306/house", max_overflow=5)
 Base = declarative_base()
-time_string = time.strftime("%Y%m%d", time.localtime())
+tb_name = "20190525_21"
 
 
-# 创建单表
 class House(Base):
-    __tablename__ = time_string
+    __tablename__ = tb_name
     id = Column(Integer, primary_key=True)
     title = Column(String(64))
     link = Column(String(64))
@@ -39,23 +37,58 @@ def drop_db():
 def insert_data_many(insert_data):
     Session = sessionmaker(bind=engine)
     session = Session()
-    for item in data_json:
-        insert_data.append(
-            House(
-                title=item['title'],
-                link=item['link'],
-                area=item['area'],
-                location=item['location'],
-                totalPrice=item['totalPrice'],
-                unitPrice=item['unitPrice'],
-                # flood = item['flood'],
-                follow=item['follow'],
-                # tag1 = item['tag1'],
-                # tag2 = item['tag']
-            )
-        )
     session.add_all(insert_data)
     session.commit()
 
 
-init_db()
+def insert_data_one_by_one(insert_data):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(insert_data)
+    session.commit()
+
+
+def txt_to_json(name):
+    # txt_name = name + ".txt"
+    # with open(txt_name, 'r', encoding='utf-8') as file:
+    #     data = file.read()
+    #
+    # data = "[" + data
+    # data = data + "]"
+    # data = data.replace(",]", "]")
+    json_save_name = "./data/" + name + "_hours.json"
+    # file = codecs.open(json_save_name, 'wb', 'utf-8')
+    # file.write(data)
+
+    insert_data = []
+    with open(json_save_name, 'r', encoding='utf-8') as f:
+        data_json = json.load(f)
+        f.close()
+
+        for house in data_json:
+            insert_h = House()
+            insert_h.title = house['title']
+            insert_h.link = house['link']
+            insert_h.area = house['area']
+            insert_h.location = house['location']
+            insert_h.totalPrice = house['totalPrice']
+            insert_h.unitPrice = house['unitPrice']
+            # insert_h.flood = house['flood']
+            insert_h.follow = house['follow']
+            # insert_h.tag1 = house['tag1']
+            # insert_h.tag2 = house['tag2']
+            insert_data.append(insert_h)
+
+    insert_data_many(insert_data)
+
+
+if __name__ == '__main__':
+    # def create_db():
+    # hous_s = House()
+    # hous_s.title = "title"
+    # hous_s.area = "area"
+    # insert_data_one_by_one(hous_s)
+    init_db()
+    # txt_to_json(tb_name)
+# data = read_json("./data/20190520_hours.json")
+# insert_data(data)
